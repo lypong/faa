2667f8863903d90fa74d203d53c431c40d976881
lypong <sfv.jacob@gmail.com>
2022-11-19 00:58:27 +0100

    Initial commit

---
+++ setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+sudo pacman -S crystal-core crystal-branding
+sudo mv /etc/pacman.conf /etc/pacman.conf.bak
+sudo cp pacman.conf /etc/pacman.conf
+cp -r kitty.conf ~/.config/kitty/
+cp -r .bashrc ~/
+ame -Sy
+ame install crystal-core crystal-branding spotify-launcher steam wqy-zenhei heroic-games-launcher lutris bitwarden kitty rustup gcc cmake make pkg-config ttf-firacode-nerd neovim wget
+rustup default stable
+LV_BRANCH='release-1.2/neovim-0.8' bash <(curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/master/utils/installer/install.sh)
+wget "https://raw.githubusercontent.com/dexpota/kitty-themes/master/themes/WildCherry.conf" -P ~/.config/kitty/kitty-themes/themes
+ln -s ~/.config/kitty/kitty-themes/themes/WildCherry.conf ~/.config/kitty/theme.conf
