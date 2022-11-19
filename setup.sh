e634fd955665c40acbd9dbcb13db0ca1556b12a3
lypong <sfv.jacob@gmail.com>
2022-11-19 18:25:31 +0100

    adding visual studio code to packages

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@ mkdir -p ~/.config/kitty/
 cp -r kitty.conf ~/.config/kitty/
 cp -r .bashrc ~/
 ame -Sy
-ame install crystal-core crystal-branding spotify-launcher steam wqy-zenhei heroic-games-launcher lutris bitwarden kitty rustup gcc cmake make pkg-config ttf-firacode-nerd neovim wget
+ame install crystal-core crystal-branding spotify-launcher steam wqy-zenhei heroic-games-launcher lutris bitwarden kitty rustup gcc cmake make pkg-config ttf-firacode-nerd neovim wget visual-studio-code-bin
 rustup default stable
 LV_BRANCH='release-1.2/neovim-0.8' bash <(curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/master/utils/installer/install.sh)
 wget "https://raw.githubusercontent.com/dexpota/kitty-themes/master/themes/WildCherry.conf" -P ~/.config/kitty/kitty-themes/themes
